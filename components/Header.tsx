21bc79c3b437c59450ad2316708c992c681cd7f3
Paul <shanit.paul@siemens.com>
2026-02-23 23:22:35 +0530

    header phone ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-40 border-b h-14 border-roseSoft/70 bg-roseLight/70 backdrop-blur">
-      <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-1 h-full">
+      <div className="mx-auto flex max-w-6xl items-center justify-between px-4 md:px-2 y-1 h-full">
         <nav className="flex items-center gap-4 text-sm">
           {navLinks.map(({ href, label, border }) => {
             const isActive = pathname === href;
@@ -29,8 +29,8 @@ export function Header() {
                 key={href}
                 href={href}
                 className={[
-                  "font-semibold transition-colors",
-                  border ? "pr-4 border-r-2" : "",
+                  "font-semibold transition-colors text-xs md:text-md",
+                  border ? "pr-2 md:pr-4 border-r-2" : "",
                   isActive
                     ? "text-rose-400 hover:text-rose-500 decoration-2"
                     : "text-slate-500 hover:text-slate-700",
@@ -43,9 +43,9 @@ export function Header() {
             );
           })}
         </nav>
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-1 md:gap-3">
           {pathname === "/gifts" && <KeptCartBadge />}
-          <Image src="/assets/tulip.png" alt="Tulip" width={24} height={32} className="object-contain mx-5" />
+          <Image src="/assets/tulip.png" alt="Tulip" width={24} height={32} className="object-contain mx-2 md:mx-5" />
         </div>
       </div>
     </header>
